a2d8dec658b030cda65bd8720c9b59c785094b01
Ana Oliveira <vianello.ana@gmail.com>
2021-06-11 13:58:27 -0300

    Primeiros testes da impressão final do programa

---
+++ GenerandoFinancas/src/generation/grupo03/finalblocoi/Impressao.java
@@ -0,0 +1,11 @@
+package generation.grupo03.finalblocoi;
+
+public class Impressao {
+
+	public static void main(String[] args) {
+		System.out.printf("%3s____________________", "");
+		System.out.printf("%n%3s %S%3s  %.2f %s", "|", "gastos", "|", 1093.6, "|");
+		System.out.printf("%n%3s———————————————————", "");
+	}
+
+}
